461c66ec06fccd84de7bb7e78aa63ef988761374
melmepham <m.a.mepham@gmail.com>
2019-12-14 13:29:42 +1300

    css styles are working as expected

--- form-validation/src/app/validators/checkbox-length.validator.ts
+++ form-validation/src/app/validators/checkbox-length.validator.ts
@@ -1,14 +1,13 @@
 import { AbstractControl } from '@angular/forms';
-import { ValidateFn } from 'codelyzer/walkerFactory/walkerFn';
 
 export function CheckboxLengthValidator(minLength: number) {
   return (control: AbstractControl) => {
     const objValues = Object.values(control.value);
     const checkboxPressed = objValues.filter(val => val);
 
-    if (checkboxPressed.length < 2) {
+    if (checkboxPressed.length < minLength) {
       return { isValidAndInView: true };
     }
     return null;
-  }
+  };
 }
